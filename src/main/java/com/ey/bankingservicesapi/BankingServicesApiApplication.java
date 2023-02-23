e1bd6b037abee6df9d530c0a97c9d50b814e10bf
derekberkley <derek.berkley@ey.com>
2023-02-23 14:46:13 -0600

    Spring Boot initializr

---
+++ src/main/java/com/ey/bankingservicesapi/BankingServicesApiApplication.java
@@ -0,0 +1,13 @@
+package com.ey.bankingservicesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingServicesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingServicesApiApplication.class, args);
+	}
+
+}
