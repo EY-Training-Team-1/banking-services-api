24804fbbdd13afa60e409b99fc10eb575c425944
vamsi <vamsia@duck.com>
2023-02-23 18:07:58 -0600

    added Lombok dependency and setup Logging

---
+++ src/main/java/com/ey/bankingservicesapi/models/LombokTest.java
@@ -0,0 +1,14 @@
+package com.ey.bankingservicesapi.models;
+
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+
+@Getter
+@Setter
+//@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
+@Slf4j
+public class LombokTest {
+    private String pCode;
+}
