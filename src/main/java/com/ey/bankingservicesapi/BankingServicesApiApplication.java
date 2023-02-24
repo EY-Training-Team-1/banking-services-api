0146aaafa526d78172a28f4bdf45f3fd4593f6fc
vamsi <vamsia@duck.com>
2023-02-23 23:28:37 -0600

    Test PR review with Github.

--- src/main/java/com/ey/bankingservicesapi/BankingServicesApiApplication.java
+++ src/main/java/com/ey/bankingservicesapi/BankingServicesApiApplication.java
@@ -10,6 +10,7 @@ public class BankingServicesApiApplication {
 
 	public static void main(String[] args) {
 		LombokTest pr = new LombokTest("sssss");
+
 		System.out.printf("pr+"+pr.getPCode());
 		SpringApplication.run(BankingServicesApiApplication.class, args);
 		LoggerTest.writeLogs();
