939df961f8b2c7d115ac31dbe851c173bd0411c9
vamsi <vamsia@duck.com>
2023-02-23 23:39:46 -0600

    code cleanup

--- src/main/java/com/ey/bankingservicesapi/BankingServicesApiApplication.java
+++ src/main/java/com/ey/bankingservicesapi/BankingServicesApiApplication.java
@@ -9,9 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BankingServicesApiApplication {
 
 	public static void main(String[] args) {
-		LombokTest pr = new LombokTest("sssss");
-
-		System.out.printf("pr+"+pr.getPCode());
 		SpringApplication.run(BankingServicesApiApplication.class, args);
 		LoggerTest.writeLogs();
 	}
