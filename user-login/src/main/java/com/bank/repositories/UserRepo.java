aa99b421ae68621cc8f4bb2ed3396c69f3f0381b
derekberkley <derek.berkley@ey.com>
2023-02-27 17:03:59 -0600

    saved progress

--- user-login/src/main/java/com/bank/repositories/UserRepo.java
+++ user-login/src/main/java/com/bank/repositories/UserRepo.java
@@ -1,5 +1,7 @@
 package main.java.com.bank.repositories;
 
+import main.java.com.bank.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
