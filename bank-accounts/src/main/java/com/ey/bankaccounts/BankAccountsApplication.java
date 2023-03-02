307fd31e06830559a596eaa13d38c72323a18e9a
vamsi <vamsia@duck.com>
2023-03-02 16:42:03 -0600

    dummy push

--- bank-accounts/src/main/java/com/ey/bankaccounts/BankAccountsApplication.java
+++ bank-accounts/src/main/java/com/ey/bankaccounts/BankAccountsApplication.java
@@ -11,7 +11,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class BankAccountsApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(BankAccountsApplication.class, args);
 	}
 
